ab34151e9ff5306a141499510eed038a54111ad9
AlexWoIf <alexey.barannikov@gmail.com>
2022-01-22 02:39:45 +0300

    Update WGauth.js

--- WGauth.js
+++ WGauth.js
@@ -394,11 +394,17 @@ function setPermission(wgid, uid) {
 											auth = true;
 											let clnt = backend.getClientByUID(row.uid);
 											if (Boolean(clnt)) {
-												channel_desc += ("[TR][TD][color=green]"+clan.members[id].account_name+"[/TD][TD][color=green]Registred[/TD][TD][color=green]"+clnt.getChannels()[0].name()+"[/TD][/TR]");
-//												channel_desc += ("[TR][TD][green]"+clan.members[id].account_name+"[/TD][TD]Not registred[/TD][TD]Not connected[/TD][/TR]");
+												channel_desc += ("[TR][TD][color=green]"+clan.members[id].account_name+"[/TD][TD][color=green]Registred[/TD][TD][color=green]"+clnt.getChannels()[0].name()+"/"+clnt.name()+"[/TD][/TR]");
+											} else {
+												offline.push(id);
+												channel_desc += ("[TR][TD][color=red]"+clan.members[id].account_name+"[/TD][TD][color=red]Registred[/TD][TD][color=red]Not connected/"+clnt.name()+"[/TD][/TR]");
 											}
 										}											
 									});
+									if ( !auth ) {
+										notAuth.push(id);
+										channel_desc += ("[TR][TD][color=black]"+clan.members[id].account_name+"[/TD][TD][color=black]Not registred[/TD][TD][color=black]Not connected/Unknown[/TD][/TR]");
+									}
 								});
 								channel_desc += "[/TABLE]";
 								toChannel.setDescription(channel_desc);
