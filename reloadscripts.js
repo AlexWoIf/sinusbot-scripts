a8c6c6632db63de72e85c77eb55d7c3bf3bd07a6
AlexWoIf <alexey.barannikov@gmail.com>
2022-01-21 03:35:38 +0300

    Create reloadscripts.js

---
+++ reloadscripts.js
@@ -0,0 +1,22 @@
+registerPlugin({
+    name: 'Scripts reloader',
+    version: '1.0a',
+    description: 'This plugin reload all script by webhook.',
+    author: 'AlexWolf <alexwolf@inbox.ru>',
+    vars: [{
+		name: 'eventName',
+	    indent: 0,
+	    title: 'Name of event for reloading scripts',
+	    type: 'string',
+	}]
+},
+
+function (sinusbot, config) {
+    const event = require('event');
+    const backend = require('backend');
+    const engine = require('engine');
+
+    event.on('public:'+config.eventName, ev => {
+		engine.reloadScripts();
+	});
+})
