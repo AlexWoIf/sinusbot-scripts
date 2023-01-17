d774315e2317954c80c2eeb25b60b98a118f714c
AlexWoIf <alexey.barannikov@gmail.com>
2023-01-18 00:59:23 +0300

    Update reloadscripts.js

--- reloadscripts.js
+++ reloadscripts.js
@@ -5,19 +5,20 @@ registerPlugin({
     description: 'This plugin reload all script by webhook.',
     author: 'AlexWolf <alexwolf@inbox.ru>',
     vars: [{
-		name: 'eventName',
-	    indent: 0,
-	    title: 'Name of event for reloading scripts',
-	    type: 'string',
-	}]
+            name: 'eventName',
+            indent: 0,
+            title: 'Name of event for reloading scripts',
+            type: 'string',
+        }
+    ]
 },
 
-function (sinusbot, config) {
+    function (sinusbot, config) {
     const event = require('event');
     const backend = require('backend');
-    const engine = require('engine');
+//    const engine = require('engine');
 
-    event.on('public:'+config.eventName, ev => {
-		engine.reloadScripts();
-	});
+    event.on('public:' + config.eventName, function() {
+        engine.reloadScripts();
+    });
 })
