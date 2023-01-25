809ab1dd73afa51d08def00b02a63bf798c31bcf
AlexWoIf <alexey.barannikov@gmail.com>
2023-01-25 19:44:17 +0300

    Update WGauth.js

--- WGauth.js
+++ WGauth.js
@@ -308,105 +308,105 @@ registerPlugin({
                                 config.channelOptions.forEach(opt => {});
                                 setClanRank(uid, channel_id, role);
                             }
-                            /*/
+                            /*
                             // Create channel if not exist using TS WebQuery
                             if (!Boolean(channel_id)) {
-                                // Replace placeholders and URLencode channel name and channel description
-                                let channel_name = encodeURIComponent(config.channelName.replace('&t', clan.tag).replace('&n', clan.name));
-                                let channel_desc = encodeURIComponent(config.channelDesc.replace('&e', "[img]" + clan.emblems.x64.wot + "[/img]").replace('&t', clan.tag).replace('&n', clan.name));
-                                http.simpleRequest({
-                                    'method': 'GET',
-                                    'url': 'http://' + config.addrTS3 + ':10080/1/channelcreate?channel_name=' + channel_name + '&channel_description=' + channel_desc + '&cpid=' + config.parentchannel,
-                                    'timeout': 6000,
-                                    'headers': {
-                                        'x-api-key': config.apikeyWebQuery
-                                    }
-                                }, function (error, response) {
-                                    if (error) {
-                                        engine.log("Error: " + error);
-                                        return;
-                                    }
-                                    if (response.statusCode != 200) {
-                                        engine.log("HTTP Error: " + response.status);
-                                        return;
-                                    }
-                                    // success!
-                                    channel_id = JSON.parse(response.data).body[0].cid;
-                                    setClanRank(uid, channel_id, role);
-                                    // Set additional channel permissions using TS WebQuery
-                                    config.channelOptions.forEach(opt => {
-                                        http.simpleRequest({
-                                            'method': 'GET',
-                                            'url': 'http://' + config.addrTS3 + ':10080/1/channeladdperm?cid=' + channel_id + '&permsid=' + opt.optionName + '&permvalue=' + opt.optionValue,
-                                            'timeout': 6000,
-                                            'headers': {
-                                                'x-api-key': config.apikeyWebQuery
-                                            }
-                                        }, function (error, response) {
-                                            if (error) {
-                                                engine.log("Error: " + error);
-                                                return;
-                                            }
-                                            if (response.statusCode != 200) {
-                                                engine.log("HTTP Error: " + response.status);
-                                                return;
-                                            }
-                                            // success!
-                                        });
-                                    });
-                                    // Create HQ subchannel
-                                    let channel_name = encodeURIComponent(config.hqChannelName);
-                                    http.simpleRequest({
-                                        'method': 'GET',
-                                        'url': 'http://' + config.addrTS3 + ':10080/1/channelcreate?channel_name=' + channel_name + '&cpid=' + channel_id,
-                                        'timeout': 6000,
-                                        'headers': {
-                                            'x-api-key': config.apikeyWebQuery
-                                        }
-                                    }, function (error, response) {
-                                        if (error) {
-                                            engine.log("Error: " + error);
-                                            return;
-                                        }
-                                        if (response.statusCode != 200) {
-                                            engine.log("HTTP Error: " + response.status);
-                                            return;
-                                        }
-                                        // success!
-                                        let hq_id = JSON.parse(response.data).body[0].cid;
-                                        //  Store new clan channel in DB
-                                        if (dbc)
-                                            dbc.exec("INSERT INTO wgchannels (clanid, channelid, hq) VALUES (?, ?, ?)", clan.clan_id, channel_id, hq_id);
-                                        setClanRank(uid, channel_id, role);
-                                        // Set additional channel permissions using TS WebQuery
-                                        config.hqChannelOptions.forEach(opt => {
-                                            http.simpleRequest({
-                                                'method': 'GET',
-                                                'url': 'http://' + config.addrTS3 + ':10080/1/channeladdperm?cid=' + hq_id + '&permsid=' + opt.optionName + '&permvalue=' + opt.optionValue,
-                                                'timeout': 6000,
-                                                'headers': {
-                                                    'x-api-key': config.apikeyWebQuery
-                                                }
-                                            }, function (error, response) {
-                                                if (error) {
-                                                    engine.log("Error: " + error);
-                                                    return;
-                                                }
-                                                if (response.statusCode != 200) {
-                                                    engine.log("HTTP Error: " + response.status);
-                                                    return;
-                                                }
-                                                // success!
-                                            });
-                                        });
-                                    });
-                                });
-                            } else {
-                                 * /
-                                                                setClanRank(uid, channel_id, role);
-                                / /
+                            // Replace placeholders and URLencode channel name and channel description
+                            let channel_name = encodeURIComponent(config.channelName.replace('&t', clan.tag).replace('&n', clan.name));
+                            let channel_desc = encodeURIComponent(config.channelDesc.replace('&e', "[img]" + clan.emblems.x64.wot + "[/img]").replace('&t', clan.tag).replace('&n', clan.name));
+                            http.simpleRequest({
+                            'method': 'GET',
+                            'url': 'http://' + config.addrTS3 + ':10080/1/channelcreate?channel_name=' + channel_name + '&channel_description=' + channel_desc + '&cpid=' + config.parentchannel,
+                            'timeout': 6000,
+                            'headers': {
+                            'x-api-key': config.apikeyWebQuery
                             }
+                            }, function (error, response) {
+                            if (error) {
+                            engine.log("Error: " + error);
+                            return;
+                            }
+                            if (response.statusCode != 200) {
+                            engine.log("HTTP Error: " + response.status);
+                            return;
+                            }
+                            // success!
+                            channel_id = JSON.parse(response.data).body[0].cid;
+                            setClanRank(uid, channel_id, role);
+                            // Set additional channel permissions using TS WebQuery
+                            config.channelOptions.forEach(opt => {
+                            http.simpleRequest({
+                            'method': 'GET',
+                            'url': 'http://' + config.addrTS3 + ':10080/1/channeladdperm?cid=' + channel_id + '&permsid=' + opt.optionName + '&permvalue=' + opt.optionValue,
+                            'timeout': 6000,
+                            'headers': {
+                            'x-api-key': config.apikeyWebQuery
+                            }
+                            }, function (error, response) {
+                            if (error) {
+                            engine.log("Error: " + error);
+                            return;
+                            }
+                            if (response.statusCode != 200) {
+                            engine.log("HTTP Error: " + response.status);
+                            return;
+                            }
+                            // success!
+                            });
+                            });
+                            // Create HQ subchannel
+                            let channel_name = encodeURIComponent(config.hqChannelName);
+                            http.simpleRequest({
+                            'method': 'GET',
+                            'url': 'http://' + config.addrTS3 + ':10080/1/channelcreate?channel_name=' + channel_name + '&cpid=' + channel_id,
+                            'timeout': 6000,
+                            'headers': {
+                            'x-api-key': config.apikeyWebQuery
+                            }
+                            }, function (error, response) {
+                            if (error) {
+                            engine.log("Error: " + error);
+                            return;
+                            }
+                            if (response.statusCode != 200) {
+                            engine.log("HTTP Error: " + response.status);
+                            return;
+                            }
+                            // success!
+                            let hq_id = JSON.parse(response.data).body[0].cid;
+                            //  Store new clan channel in DB
+                            if (dbc)
+                            dbc.exec("INSERT INTO wgchannels (clanid, channelid, hq) VALUES (?, ?, ?)", clan.clan_id, channel_id, hq_id);
+                            setClanRank(uid, channel_id, role);
+                            // Set additional channel permissions using TS WebQuery
+                            config.hqChannelOptions.forEach(opt => {
+                            http.simpleRequest({
+                            'method': 'GET',
+                            'url': 'http://' + config.addrTS3 + ':10080/1/channeladdperm?cid=' + hq_id + '&permsid=' + opt.optionName + '&permvalue=' + opt.optionValue,
+                            'timeout': 6000,
+                            'headers': {
+                            'x-api-key': config.apikeyWebQuery
+                            }
+                            }, function (error, response) {
+                            if (error) {
+                            engine.log("Error: " + error);
+                            return;
+                            }
+                            if (response.statusCode != 200) {
+                            engine.log("HTTP Error: " + response.status);
+                            return;
+                            }
+                            // success!
+                            });
+                            });
+                            });
+                            });
+                            } else {
 
+                             */
+
+                            setClanRank(uid, channel_id, role);
+                            //}
                         }
                     });
             }
