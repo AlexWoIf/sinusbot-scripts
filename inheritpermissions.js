f086abcdc4196e37c15756a682e0130353f990de
AlexWoIf <alexey.barannikov@gmail.com>
2023-04-06 12:09:21 +0300

    Update inheritpermissions.js

--- inheritpermissions.js
+++ inheritpermissions.js
@@ -37,7 +37,7 @@ registerPlugin({
         if (parent != null) {
             let parentPerms = {};
             parent.getPermissions().forEach(perm => {
-                engine.log(perm.Name,perm.Value);
+                engine.log(perm.ChannelPermission.Name,perm.ChannelPermission.Value);
                 parentPerms[perm.Name] = perm.Value;
             });
             engine.log(parentPerms);
