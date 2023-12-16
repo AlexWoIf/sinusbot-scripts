63b5851a88b63c97617bc63aaf04a79a3a452b8e
AlexWoIf <alexey.barannikov@gmail.com>
2023-12-16 17:39:13 +0300

    Create iglkz.js

---
+++ iglkz.js
@@ -0,0 +1,35 @@
+registerPlugin({
+    name: 'IGL.KZ teleport',
+    version: '1.0a',
+    description: 'This plugin provide some matchmaking functions to Counterstrike2.',
+    author: 'AlexWolf <alexwolf@inbox.ru>',
+    vars: [{
+            name: 'entryChannel',
+            title: 'Teleport channel',
+            indent: 0,
+            type: 'channel'
+        }, {
+            name: 'authSiteURL',
+            title: 'Site API url',
+            indent: 0,
+            type: 'string',
+            placeholder: 'http://sitename.com/teamspeak/'
+        },
+    ]
+},
+
+    function (sinusbot, config) {
+    var event = require('event');
+    var backend = require('backend');
+    const engine = require('engine');
+
+    event.on('clientMove', function (ev) {
+        if (ev.client.isSelf()) {
+            return;
+        }
+        var userChannel = ev.toChannel;
+        if (userChannel == config.entryChannel) {
+            ev.client.poke("Ссылка для авторизации: "+config.authSiteURL);
+        }
+    });
+})
